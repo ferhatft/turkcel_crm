d12697f59c338384625234aba26f02cc1bc46375
Enes Ekşi <enes-eksi@hotmail.com>
2024-03-16 15:37:00 +0300

    First Commit Base Created

---
+++ customerservice/src/main/java/com/turkcell/pair6/customerservice/CustomerserviceApplication.java
@@ -0,0 +1,17 @@
+package com.turkcell.pair6.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerserviceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CustomerserviceApplication.class, args);
+	}
+
+	//CustomerService
+	//DB Tasarımı
+	//docker-compose' CustomerService için gerekli konfigürasyonlar
+}
---
+++ productservice/src/main/java/com/turkcell/pair6/productservice/ProductserviceApplication.java
@@ -0,0 +1,16 @@
+package com.turkcell.pair6.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductserviceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProductserviceApplication.class, args);
+	}
+
+
+
+}
